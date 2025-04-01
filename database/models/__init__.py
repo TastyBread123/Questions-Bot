e49bb58b1bddac8b2ee8a5af22e1c3d171877082
TastyBread123 <117034601+TastyBread123@users.noreply.github.com>
2025-04-01 23:05:21 +0300

    1.1

--- database/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from database.models.answer import Answer
-from database.models.contest import Contest
-from database.models.question import Question
-from database.models.result import Result
-from database.models.user import User
