0f28a6a0327011151aae6907983a18140fcb0497
TastyBread123 <117034601+TastyBread123@users.noreply.github.com>
2025-04-01 23:05:57 +0300

    1.1

--- handlers/commands/__init__.py
+++
@@ -1,7 +0,0 @@
-from handlers.commands.start import router as start_router
-from handlers.commands.profile import router as profile_router
-from handlers.commands.create_contest.create_contest import router as cr_contest_router
-from handlers.commands.create_contest.create_question import router as cr_question_router
-from handlers.commands.play_contest.id_enter import router as id_enter_router
-
-routers = [start_router, profile_router, cr_contest_router, cr_question_router, id_enter_router]
